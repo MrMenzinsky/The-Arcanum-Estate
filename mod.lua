f83db1af56d628d96cf67548a472fb834e3ddb3e
Anton Menzinsky <17617000+Frommarn@users.noreply.github.com>
2025-10-18 23:31:29 +0200

    Quick fix: Make sure that resources load first to get custom enums registered before anything else

--- mod.lua
+++ mod.lua
@@ -1,13 +1,16 @@
 local mod = foundation.createMod();
 
+-- Add a new enum value to restrict which villagers can go through the monument buildings.
+mod:registerEnumValue("NAVMESH_LOCK_CATEGORY", "ARCANE")
+
+-- Resources and their assets (icons)
+mod:dofile("scripts/resources.lua")
+
 -- Custom Building Function: BUILDING_FUNCTION_CHANGE_PROFILE_WORKPLACE
 mod:dofile("scripts/building_function_change_profile_workplace.lua")
 
 -- Estate, progress path, and privilege assets
 mod:dofile("scripts/arcanum_estate.lua")
-
--- Resources and their assets (icons)
-mod:dofile("scripts/resources.lua")
 
 -- Jobs and statuses
 mod:dofile("scripts/villager_jobs.lua")
@@ -26,7 +29,4 @@ mod:registerAssetId("icons/Icon_3.png", "ICON_3", "ATLAS_CELL") -- HeaderImage
 mod:registerAssetId("icons/Icon_4.png", "ICON_4", "ATLAS_CELL") -- 
 mod:registerAssetId("icons/Icon_5.png", "ICON_5", "ATLAS_CELL") -- Privilege Image
 
-
--- Add a new enum value to restrict which villagers can go through the monument buildings.
-mod:registerEnumValue("NAVMESH_LOCK_CATEGORY", "ARCANE")
 
