706e12cfae0414b7f0cc4c03cb4a30e73997da26
Anton Menzinsky <anton.menzinsky@asurgent.se>
2025-12-14 13:51:33 +0100

    Add more template parts

--- scripts/aethercourt/monument_parts.lua
+++ scripts/aethercourt/monument_parts.lua
@@ -1,17 +1,44 @@
 local mod = ...
 
 local filename = "small_house_003.fbx"
+
+
 -- To easily read in and correctly parse the different attach points (Attach_Bottom, Attach_Top etc.)
 mod:registerAssetProcessor("models/" .. filename, {
     DataType = "BUILDING_ASSET_PROCESSOR"
 })
 
 
--- Build parts
-mod:registerAssetId("models/" .. filename .. "/Prefab/Big_House_Part", "PREFAB_BIG_HOUSE_PART")
-mod:registerAssetId("models/" .. filename .. "/Prefab/Small_House_Part", "PREFAB_SMALL_HOUSE_PART")
+-- -- Build parts
+-- mod:registerAssetId("models/" .. filename .. "/Prefab/Big_House_Part", "PREFAB_BIG_HOUSE_PART")
+-- mod:registerAssetId("models/" .. filename .. "/Prefab/Small_House_Part", "PREFAB_SMALL_HOUSE_PART")
 
 
+
+local templateBlocks = {
+    "1x1.5m_Flat_Part",
+    "1x1m_Flat_Part",
+    "1x1m_Part",
+    "2x2m_Part",
+    "4x4m_Part",
+    "8x8m_Part",
+    "Big_House_Part",
+    "Small_House_Part"
+}
+
+for _, templateBlock in ipairs(templateBlocks) do
+    local prefab = "PREFAB_" .. string.upper(templateBlock)
+    mod:registerAssetId("models/" .. filename .. "/Prefab/" .. templateBlock, prefab)
+
+    mod:registerAsset({
+        DataType = "BUILDING_PART",
+        Id = string.upper(templateBlock),
+        ConstructorData = {
+            DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
+            CoreObjectPrefab = prefab
+        }
+    })
+end
 
 -- Root (main) part
 -- mod:registerAsset({
@@ -48,20 +75,20 @@ mod:registerAssetId("models/" .. filename .. "/Prefab/Small_House_Part", "PREFAB
 --     }
 -- })
 
-mod:registerAsset({
-    DataType = "BUILDING_PART",
-    Id = "BIG_HOUSE_PART",
-    ConstructorData = {
-        DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
-        CoreObjectPrefab = "PREFAB_BIG_HOUSE_PART"
-    }
-})
+-- mod:registerAsset({
+--     DataType = "BUILDING_PART",
+--     Id = "BIG_HOUSE_PART",
+--     ConstructorData = {
+--         DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
+--         CoreObjectPrefab = "PREFAB_BIG_HOUSE_PART"
+--     }
+-- })
 
-mod:registerAsset({
-    DataType = "BUILDING_PART",
-    Id = "SMALL_HOUSE_PART",
-    ConstructorData = {
-        DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
-        CoreObjectPrefab = "PREFAB_SMALL_HOUSE_PART"
-    }
-})
+-- mod:registerAsset({
+--     DataType = "BUILDING_PART",
+--     Id = "SMALL_HOUSE_PART",
+--     ConstructorData = {
+--         DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
+--         CoreObjectPrefab = "PREFAB_SMALL_HOUSE_PART"
+--     }
+-- })
